14b091d4489b886a595862d1767950b1b09eef08
Jan Schulte <hello@unexpected-co.de>
2017-05-26 13:24:20 +0200

    Persist user's GitHub OAuth Token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import { saveToken, getToken } from './tokenStore';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      hasToken: false
+    };
+  }
+
+  componentDidMount() {
+    const hasToken = !!getToken();
+    this.setState({ hasToken: hasToken });
+  }
+
   getRepositories() {
     return [
       "Foo",
@@ -20,26 +35,54 @@ class App extends Component {
     ];
   }
 
+  onSaveToken() {
+    const token = this.refs.githubToken.value;
+    saveToken(token);
+    this.setState({ hasToken: true });
+  }
+
   render() {
+    const hasToken = this.state.hasToken;
+
+    var configurationClassNames;
+
+    if (hasToken) {
+      configurationClassNames = "configuration hidden";
+    }
+    else {
+      configurationClassNames = "configuration visible";
+    }
+
     return (
       <div className="App">
-        <div className="repo-list">
-          <ul>
-            {this.getRepositories().map(r => (
-              <li className="repository">
-                {r}
-              </li>
-            ))}
-          </ul>
+        <h3>Configure your GitHub OAuth token</h3>
+        <div className={configurationClassNames}>
+          <input type="text" ref="githubToken" className="githubToken" />
+          <input type="button"
+                  value="Save Token"
+                  onClick={() => this.onSaveToken()}
+                  ref="saveGitHubToken"
+                  className="saveToken" />
         </div>
-        <div className="repository-issues">
-          <ul className="issues">
-            {this.getIssues().map(i => (
-              <li className="issue">
-                {i}
-              </li>
-            ))}
-          </ul>
+        <div class="repos">
+          <div className="repo-list">
+            <ul>
+              {this.getRepositories().map(r => (
+                <li className="repository">
+                  {r}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="repository-issues">
+            <ul className="issues">
+              {this.getIssues().map(i => (
+                <li className="issue">
+                  {i}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     );
