e93e8a971f4d8d1cfafec15d1781b90458cfe963
Jan Schulte <hello@unexpected-co.de>
2017-06-04 23:00:15 +0200

    Updates

--- src/tokenStore.js
+++
@@ -1,9 +0,0 @@
-const Identifier = 'githubtoken';
-
-export function saveToken(token) {
-  localStorage.setItem(Identifier, token);
-}
-
-export function getToken() {
-  return localStorage.getItem(Identifier);
-}
